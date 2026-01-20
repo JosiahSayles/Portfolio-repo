998fd163a452868f6a1b11850e48debed7202f40
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-01-19 19:09:30 -0700

    Adding a video background for homepage and styling

--- App.jsx
+++ App.jsx
@@ -1,1 +1,19 @@
-import { createRoot } from "react-dom/client";
+import { Routes, Route } from "react";
+import Layout from "./layout/Layout";
+import HomePage from "./Components/Home-page";
+import AboutPage from "./Components/About-page";
+import Portfolio from "./Components/Portfolio";
+import ContactPage from "./Components/Contact-page";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/About" element={<AboutPage />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/Contact" element={<ContactPage />} />
+      </Route>
+    </Routes>
+  );
+}
